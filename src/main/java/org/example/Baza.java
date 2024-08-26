07b1887660efbd09545ec9fd3b568e52034efc33
Vladislav Kolyagin <vladislavkolyagin@yandex.ru>
2024-08-26 23:41:58 +0300

    добавил класс

---
+++ src/main/java/org/example/Baza.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Baza {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public Baza(int size) {
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+
+
+    }
+}
